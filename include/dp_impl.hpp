814a82e868f95ca120e0640c7a26d14e18ec3195
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-02-09 16:16:36 -0500

    removing this-> for clarity where possible

--- include/dp_impl.hpp
+++ include/dp_impl.hpp
@@ -5,18 +5,18 @@ VarDP::VarDP(const std::vector<VXd>& train_data, const std::vector<VXd>& test_da
 	//copy in the model
 	this->model = model;
 	this->K = K;
-	this->M = model.getStatDimension();
-	this->N = train_data.size();
-	this->Nt = test_data.size();
+	M = model.getStatDimension();
+	N = train_data.size();
+	Nt = test_data.size();
 
 	//compute exponential family statistics once
-	this->train_stats = MXd::Zero(this->N, this->M);
-	this->test_stats = MXd::Zero(this->Nt, this->M);
-	for (uint32_t i = 0; i < this->N; i++){
-		this->train_stats.row(i) = model.getStat(train_data[i]).transpose();
+	train_stats = MXd::Zero(N, M);
+	test_stats = MXd::Zero(Nt, M);
+	for (uint32_t i = 0; i < N; i++){
+		train_stats.row(i) = model.getStat(train_data[i]).transpose();
 	}
-	for (uint32_t i = 0; i < this->Nt; i++){
-		this->test_stats.row(i) = model.getStat(test_data[i]).transpose();
+	for (uint32_t i = 0; i < Nt; i++){
+		test_stats.row(i) = model.getStat(test_data[i]).transpose();
 	}
 
 	//initialize the random device
@@ -24,19 +24,19 @@ VarDP::VarDP(const std::vector<VXd>& train_data, const std::vector<VXd>& test_da
 	if (rd.entropy() == 0){
 		std::cout << "WARNING: ENTROPY 0, NOT SEEDING THE RANDOM GEN PROPERLY" << std::endl;
 	}
-	this->rng.seed(rd());
+	rng.seed(rd());
 
 	//initialize memory
-	this->a = this->b = this->psisum = this->nu = this->sumzeta = this->dlogh_dnu = this->logh = VXd::Zeros(this->K);
-	this->zeta = MXd::Zeros(this->N, this->K);
-	this->sumzetaT = this->dlogh_deta = this->eta = MXd::Zeros(this->K, this->M);
+	a = b = psisum = nu = sumzeta = dlogh_dnu = logh = VXd::Zeros(K);
+	zeta = MXd::Zeros(N, K);
+	sumzetaT = dlogh_deta = eta = MXd::Zeros(K, M);
 }
 
 void VarDP::run(bool computeTestLL, double tol){
 	//clear any previously stored results
-	this->times.clear();
-	this->objs.clear();
-	this->testlls.clear();
+	times.clear();
+	objs.clear();
+	testlls.clear();
 
 	//create objective tracking vars
 	double diff = 10.0*tol + 1.0;
@@ -48,28 +48,28 @@ void VarDP::run(bool computeTestLL, double tol){
 	cpuTime.start();
 
 	//initialize the variables
-	this->initWeightsParams();
-	this->updateLabelDist();
+	initWeightsParams();
+	updateLabelDist();
 
 	//loop on variational updates
 	while(diff > tol){
-		this->updateWeightDist();
-		this->updateParamDist();
-		this->updateLabelDist();
+		updateWeightDist();
+		updateParamDist();
+		updateLabelDist();
 		prevobj = obj;
 		//store the current time
-		this->times.push_back(cpuTime.get());
+		times.push_back(cpuTime.get());
 		//compute the objective
-		obj = this->computeObjective();
+		obj = computeObjective();
 		//save the objective
-		this->objs.push_back(obj);
+		objs.push_back(obj);
 		//compute the obj diff
 		diff = (obj - prevobj)/obj;
 		//if test likelihoods were requested, compute those (but pause the timer first)
 		if (computeTestLL){
 			cpuTime.stop();
-			double testll = this->computeTestLL();
-			this->testlls.push_back(testll);
+			double testll = computeTestLL();
+			testlls.push_back(testll);
 			cpuTime.start();
 		}
 	}
@@ -79,34 +79,34 @@ void VarDP::run(bool computeTestLL, double tol){
 
 void VarDP::initWeightsParams(){
 	//create random statistics from data collection
-	std::uniform_int_distribution unii(0, this->N);
-	MXd random_sumzeta = 5.0*(1.0+MXd::Random(1, this->K))/2.0;
-	MXd random_sumzetaT = MXd::Zero(this->K, this->M);
-	for (uint32_t k = 0; k < this->K; k++){
-		random_sumzetaT.row(k) = this->train_stats.row(unii(this->rng))*random_sumzeta(k);
+	std::uniform_int_distribution unii(0, N);
+	MXd random_sumzeta = 5.0*(1.0+MXd::Random(1, K))/2.0;
+	MXd random_sumzetaT = MXd::Zero(K, M);
+	for (uint32_t k = 0; k < K; k++){
+		random_sumzetaT.row(k) = train_stats.row(unii(rng))*random_sumzeta(k);
 	}
 
 	//get psi and eta from that
 	double psibk = 0.0;
-	for (uint32_t k = 0; k < this->K; k++){
+	for (uint32_t k = 0; k < K; k++){
 		//update weights
-		this->a(k) = 1.0+random_sumzeta(k);
-		this->b(k) = model.getAlpha();
-		for (uint32_t j = k+1; j < this->K; j++){
-			this->b(k) += random_sumzeta(k);
+		a(k) = 1.0+random_sumzeta(k);
+		b(k) = model.getAlpha();
+		for (uint32_t j = k+1; j < K; j++){
+			b(k) += random_sumzeta(k);
 		}
     	double psiak = boost_psi(a(k)) - boost_psi(a(k)+b(k));
-    	this->psisum(k) = psiak + psibk;
+    	psisum(k) = psiak + psibk;
     	psibk += boost_psi(b(k)) - boost_psi(a(k)+b(k));
 
 	    //Update the parameters
-	    for (uint32_t j = 0; j < this->M; j++){
-	    	this->eta(k, j) = model.getEta0()(j)+random_sumzetaT(k, j);
+	    for (uint32_t j = 0; j < M; j++){
+	    	eta(k, j) = model.getEta0()(j)+random_sumzetaT(k, j);
 	    }
-		this->nu(k) = model.getNu0() + random_sumzeta(k);
+		nu(k) = model.getNu0() + random_sumzeta(k);
 	}
 	//update logh/etc
-	this->model.getLogH(this->eta, this->nu, this->logh, this->dlogh_deta, this->dlogh_dnu);
+	model.getLogH(eta, nu, logh, dlogh_deta, dlogh_dnu);
 	return;
 
 }
@@ -114,14 +114,14 @@ void VarDP::initWeightsParams(){
 void VarDP::updateWeightDist(){
 	//Update a, b, and psisum
 	double psibk = 0.0;
-	for (uint32_t k = 0; k < this->K; k++){
-		this->a(k) = 1.0+this->sumzeta(k);
-		this->b(k) = model.getAlpha();
-		for (uint32_t j = k+1; j < this->K; j++){
-			this->b(k) += this->sumzeta(j);
+	for (uint32_t k = 0; k < K; k++){
+		a(k) = 1.0+sumzeta(k);
+		b(k) = model.getAlpha();
+		for (uint32_t j = k+1; j < K; j++){
+			b(k) += sumzeta(j);
 		}
     	double psiak = boost_psi(a(k)) - boost_psi(a(k)+b(k));
-    	this->psisum(k) = psiak + psibk;
+    	psisum(k) = psiak + psibk;
     	psibk += boost_psi(b(k)) - boost_psi(a(k)+b(k));
 	}
 	return;
@@ -129,38 +129,38 @@ void VarDP::updateWeightDist(){
 
 void VarDP::updateParamDist(){
 	//Update the parameters
-	for (uint32_t k = 0; k < this->K; k++){
-	    for (uint32_t j = 0; j < this->M; j++){
-	    	this->eta(k, j) = model.getEta0()(j)+sumzetaT(k, j);
+	for (uint32_t k = 0; k < K; k++){
+	    for (uint32_t j = 0; j < M; j++){
+	    	eta(k, j) = model.getEta0()(j)+sumzetaT(k, j);
 	    }
-	    this->nu(k) = model.getNu0() + sumzeta(k);
+	    nu(k) = model.getNu0() + sumzeta(k);
 	}
 	//update logh/etc
-	this->model.getLogH(this->eta, this->nu, this->logh, this->dlogh_deta, this->dlogh_dnu);
+	model.getLogH(eta, nu, logh, dlogh_deta, dlogh_dnu);
 	return;
 }
 
 void VarDP::updateLabelDist(){
 	//update the label distribution
-	for (uint32_t i = 0; i < this->N; i++){
+	for (uint32_t i = 0; i < N; i++){
 		//compute the log of the weights, storing the maximum so far
 		double logpmax = -std::numeric_limits<double>::infinity();
-		for (uint32_t k = 0; k < this->K; k++){
-			this->zeta(i, k) = this->psisum(k) - this->dlogh_dnu(k);
-			for (uint32_t j = 0; j < this->M; j++){
-				this->zeta(i, k) -= this->train_stats(i, j)*dlogh_deta(k, j);
+		for (uint32_t k = 0; k < K; k++){
+			zeta(i, k) = psisum(k) - dlogh_dnu(k);
+			for (uint32_t j = 0; j < M; j++){
+				zeta(i, k) -= train_stats(i, j)*dlogh_deta(k, j);
 			}
 			logpmax = (zeta(i, k) > logpmax ? zeta(i, k) : logpmax);
 		}
 		//make numerically stable by subtracting max, take exp, sum them up
 		double psum = 0.0;
-		for (uint32_t k = 0; k < this->K; k++){
+		for (uint32_t k = 0; k < K; k++){
 			zeta(i, k) -= logpmax;
 			zeta(i, k) = exp(zeta(i, k));
 			psum += zeta(i, k);
 		}
 		/*normalize*/
-		for (uint32_t k = 0; k < this->K; k++){
+		for (uint32_t k = 0; k < K; k++){
 			zeta(i, k) /= psum;
 		}
 	}
@@ -179,14 +179,6 @@ VarDPResults VarDP::getResults(){
 	return dpr;
 }
 
-void VarDP::getModelResults(MXd& zeta, MXd& eta, VXd& a, VXd& b){
-	zeta = this->zeta;
-	eta = this->eta;
-	a = this->a;
-	b = this->b;
-	return;
-}
-
 
 double VarDP::computeObjective(){
 
@@ -197,7 +189,7 @@ double VarDP::computeObjective(){
 
 	//get the variational beta entropy
 	double betaEntropy = 0.0;
-	for (uint32_t k = 0; k < this->K; k++){
+	for (uint32_t k = 0; k < K; k++){
         betaEntropy += -boost_lbeta(a(k), b(k)) + (a(k)-1.0)*boost_psi(a(k)) +(b(k)-1.0)*boost_psi(b(k))-(a(k)+b(k)-2.0)*boost_psi(a(k)+b(k));
 	}
 
