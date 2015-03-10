e65ab2ea92b729db1248f0158c202a6fc0bfaada
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-03-10 12:48:20 -0400

    added alpha to plots based on wt

--- test/dpplot.py
+++ test/dpplot.py
@@ -7,9 +7,8 @@ eta = np.genfromtxt('dpmix-eta.log')
 nu = np.genfromtxt('dpmix-nu.log')
 ab = np.genfromtxt('dpmix-ab.log')
 
-stick = 1.0
-for i in range(ab.shape[1]):
 
+stick = 1.0
 D = train_data.shape[1]
 
 plt.figure()
@@ -24,8 +23,11 @@ for i in range(eta.shape[0]):
     xi = eta[i, D*D+D]-D-2
     sig = psi/(xi+D+1)
 
+    wt = stick*ab[0, i]/(ab[0, i]+ab[1, i])
+    stick = stick*ab[1, i]/(ab[0, i]+ab[1, i])
+
     xy  = mu[:, np.newaxis] + np.dot(np.linalg.cholesky(sig), np.vstack((np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)))))
-    plt.plot(xy[0, :], xy[1, :], lw=5)
+    plt.plot(xy[0, :], xy[1, :], lw=5, alpha=wt)
 
 plt.axes().set_aspect('equal')
 plt.show()
--- test/hdpplot.py
+++ test/hdpplot.py
@@ -7,6 +7,8 @@ eta = np.genfromtxt('hdpmix-eta.log')
 nu = np.genfromtxt('hdpmix-nu.log')
 uv = np.genfromtxt('hdpmix-uv.log')
 
+
+stick = 1.0
 D = train_data.shape[1]
 
 plt.figure()
@@ -21,8 +23,12 @@ for i in range(eta.shape[0]):
     xi = eta[i, D*D+D]-D-2
     sig = psi/(xi+D+1)
 
+    wt = stick*uv[0, i]/(uv[0, i]+uv[1, i])
+    stick = stick*uv[1, i]/(uv[0, i]+uv[1, i])
+
+
     xy  = mu[:, np.newaxis] + np.dot(np.linalg.cholesky(sig), np.vstack((np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)))))
-    plt.plot(xy[0, :], xy[1, :], lw=5)
+    plt.plot(xy[0, :], xy[1, :], lw=5, alpha=wt)
 
 plt.axes().set_aspect('equal')
 plt.show()
