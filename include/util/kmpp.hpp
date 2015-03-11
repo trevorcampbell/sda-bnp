70581100ae7b31baae957030f19a6719049cedbf
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-03-11 11:42:40 -0400

    templated distance for kmpp

--- include/util/kmpp.hpp
+++ include/util/kmpp.hpp
@@ -2,17 +2,21 @@
 #include <Eigen/Dense>
 #include <vector>
 #include <random>
+#include <limits>
 typedef Eigen::VectorXd VXd;
 typedef Eigen::MatrixXd MXd;
 
-std::vector<uint32_t> kmeanspp(MXd x, uint32_t K, std::mt19937& rng){
+//Dsq is a function that takes two eigen vectors as outputs their "distance" squared
+//"distance" could be a bregman divergence sq, or euclidean dist squared, etc
+template<typename Func>
+std::vector<uint32_t> kmeanspp(MXd x, Func Dsq, uint32_t K, std::mt19937& rng){
 	std::uniform_int_distribution<> uniint(0, x.rows()-1);
 	std::vector<uint32_t> res;
 	res.push_back(uniint(rng));
-	std::vector<double> mindists(std::numeric_limits<double>::infinity(), x.rows());
+	std::vector<double> mindists(x.rows(), std::numeric_limits<double>::infinity());
 	for (uint32_t i =1; i < K; i++){
 		for (uint32_t j = 0; j < x.rows(); j++){
-			double newdist = (x.row(j)-x.row(res.back())).squaredNorm();
+			double newdist = Dsq(x.row(j).transpose(), x.row(res.back()).transpose());
 			mindists[j] = mindists[j] < newdist ? mindists[j] : newdist;
 		}
 		std::discrete_distribution<> disc(mindists.begin(), mindists.end());
