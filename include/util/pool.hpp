fa63ffe103c3443bdb7d5a32136f7bb7d4d34bc6
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-03-12 11:44:52 -0400

    done writing worker pool code

--- include/util/pool.hpp
+++ include/util/pool.hpp
@@ -5,34 +5,33 @@
 #include<mutex>
 #include<condition_variable>
 
-template<typename Alg>
-class SDA{
+template<typename Job>
+class Pool{
 	public:
-		SDA(uint32_t nThr);
-		~SDA();
-		void run();
+		Pool(uint32_t nThr);
+		~Pool();
+		void submit(Job job);
 	private:
 		std::vector< std::thread > workers;
 		void worker();
 		bool stop;
 
-		std::deque< job > jobs;
-		std::mutex model_mutex, queue_mutex;
+		std::deque<Job> jobs;
+		std::mutex queue_mutex;
 		std::condition_variable queue_cond;
 };
 
 
-
-template<typename Alg>
-SDA<Alg>::SDA(uint32_t nThr){
+template<typename Job>
+Pool<Job>::Pool(uint32_t nThr){
 	stop = false;
 	for(uint32_t t = 0; t < nThr; t++){
-		workers.push_back(std::thread(&SDA<Alg>::worker, this));
+		workers.push_back(std::thread(&Pool<Job>::worker, this));
 	}
 }
 
-template<typename Alg>
-SDA<Alg>::~SDA(){
+template<typename Job>
+Pool<Job>::~Pool(){
 	stop = true;
 	queue_cond.notify_all();
 	for(uint32_t i = 0; i < workers.size(); i++){
@@ -40,15 +39,18 @@ SDA<Alg>::~SDA(){
 	}
 }
 
-template<typename Alg>
-SDA<Alg>::run(){
-
-	//TODO: add jobs to the queue
+template<typename Job>
+void Pool<Job>::submit(Job job){
+	{
+		std::lock_guard<std::mutex> lock(queue_mutex);
+		jobs.push_back(job);
+	}
+	queue_cond.notify_one();
 }
 
 
-template<typename Alg>
-void SDA<Alg>::worker(){
+template<typename Job>
+void Pool<Job>::worker(){
 	while(true){
 		{
 			std::lock_guard<std::mutex> lock(queue_mutex);
@@ -64,7 +66,7 @@ void SDA<Alg>::worker(){
 			jobs.pop_front();
 		}//release the lock_guard
 
-		//TODO: do the job
+		job();//do the job
 	}
 }
 #define __POOL_HPP
