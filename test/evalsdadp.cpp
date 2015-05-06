6904b8b8385c68db344f1cf11c217366cc28813b
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-05-06 12:37:05 -0400

    removed debugging output

--- test/evalsdadp.cpp
+++ test/evalsdadp.cpp
@@ -21,14 +21,14 @@ typedef Eigen::VectorXd VXd;
 
 int main(int argc, char** argv){
 	//constants
-	uint32_t K = 3;
-	uint32_t Knew = 3;
-	uint32_t N = 1000;
-	uint32_t Nmini = 100;
-	uint32_t Nt = 100;
+	uint32_t K = 40;
+	uint32_t Knew = 10;
+	uint32_t N = 100000;
+	uint32_t Nmini = 1000;
+	uint32_t Nt = 10000;
 	uint32_t D = 2;
 	double alpha = 1.0;
-	uint32_t monteCarloTrials = 3;
+	uint32_t monteCarloTrials = 1;
 	std::vector<uint32_t> Nthr;
 	Nthr.push_back(1);
 	Nthr.push_back(2);
