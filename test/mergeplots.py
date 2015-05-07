d00b03161b234bc3cb1ea9c682562f753d49fa5d
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-05-07 16:24:29 -0400

    added plotting of merge sequence

--- test/mergeplots.py
+++ test/mergeplots.py
@@ -10,57 +10,64 @@ font = {'family' : 'normal',
         'size' : 24}
 matplotlib.rc('font', **font)
 
+model = np.genfromtxt('model-000.log')
+train_data = np.genfromtxt('train-000.log')
+test_data = np.genfromtxt('test-000.log')
+D = int((-1 + np.sqrt(1-4*(1-model.shape[1])))/2)
+plt.figure()
+plt.scatter(train_data[:, 0], train_data[:, 1], c='b')
+plt.scatter(test_data[:, 0], test_data[:, 1], c='g')
+for k in range(model.shape[0]):
+    mu = model[k, 0:D]
+    sig = np.zeros((D, D))
+    for j in range(D):
+        sig[j, :] = model[k, (j+1)*D:(j+2)*D]
+    wt = model[k, -1]
+    xy  = mu[:, np.newaxis] + np.dot(np.linalg.cholesky(sig), np.vstack((np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)))))
+    plt.plot(xy[0, :], xy[1, :], lw=5, alpha=np.sqrt(wt), c='r')
+plt.axes().set_aspect('equal')
+plt.xlim((-100, 100))
+plt.ylim((-100, 100))
+plt.title('True Data/Model')
 
 
-#create the plot output directory
-sdabasename = 'sdadpmix'
-batchbasename = 'vardpmix'
-oldbatchbasename = 'vardpmixold'
-svabasename = 'svadpmix'
-svibasename = 'svidpmix'
-movbbasename = 'movbdpmix'
-outdir = 'plots'
-if not os.path.exists(outdir):
-    os.makedirs(outdir)
 
-#get the mcmc run/nthr tags
-mcmc_run_tags = []
-nthr_tags = []
+#get the name tags
+dist_tags = []
 for f in os.listdir('.'):
-    if f[:6] == 'model-':
-        mcmc_run_tags.append(f[6:-4])
-for f in os.listdir('.'):
-    if f[:14] == sdabasename+'-nThr_' and f[-7:] == '-ab.log':
-        for tag in mcmc_run_tags:
-            if len(tag) < len(f[14:-7]) and f[-7-len(tag):-7] == tag:
-                nthr_tags.append(f[14:-7-len(tag)-1])
-mcmc_run_tags = sorted(mcmc_run_tags)
-nthr_tags = sorted(list(set(nthr_tags)))
+    if f[:6] == 'dist0-' or f[:6] == 'dist1-' or f[:7] == 'dist1r-' or f[:6] == 'dist2-' or f[:6] == 'distf-':
+        first_dash = 0
+        second_dash = 0
+        first_dash = f.find('-')
+        second_dash = f[first_dash+1:].find('-')
+        dist_tags.append(f[first_dash+1:second_dash+first_dash+1])
 
+dist_tags = sorted(list(set(dist_tags)))
+dtype = ['0', '1', '1r', '2', 'f']
 
-##Plot 1: data and model for each mcmc run
-#for mtag in mcmc_run_tags:
-#    model = np.genfromtxt('model-'+mtag+'.log')
-#    train_data = np.genfromtxt('train-'+mtag+'.log')
-#    test_data = np.genfromtxt('test-'+mtag+'.log')
-#
-#    D = int((-1 + np.sqrt(1-4*(1-model.shape[1])))/2)
-#
-#    plt.figure()
-#    plt.scatter(train_data[:, 0], train_data[:, 1], c='b')
-#    plt.scatter(test_data[:, 0], test_data[:, 1], c='g')
-#    for k in range(model.shape[0]):
-#        mu = model[k, 0:D]
-#        sig = np.zeros((D, D))
-#        for j in range(D):
-#            sig[j, :] = model[k, (j+1)*D:(j+2)*D]
-#        wt = model[k, -1]
-#        xy  = mu[:, np.newaxis] + np.dot(np.linalg.cholesky(sig), np.vstack((np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)))))
-#        plt.plot(xy[0, :], xy[1, :], lw=5, alpha=np.sqrt(wt), c='r')
-#    plt.axes().set_aspect('equal')
-#    plt.xlim((-100, 100))
-#    plt.ylim((-100, 100))
-#    plt.savefig(outdir+'/model-'+mtag+'.pdf')
+for i in range(len(dist_tags)):
+    for j in range(len(dtype)):
+        plt.figure()
+        eta = np.genfromtxt('dist'+dtype[j]+'-'+dist_tags[i]+'-eta.log'))
+        nu = np.genfromtxt('dist'+dtype[j]+'-'+dist_tags[i]+'-nu.log'))
+        ab = np.genfromtxt('dist'+dtype[j]+'-'+dist_tags[i]+'-ab.log'))
 
+        stick = 1.0
+        for k in range(eta.shape[0]):
+            mu = eta[k, D*D:D*D+D]/nu[k]
+            psi = np.zeros((D, D))
+            for m in range(D):
+                psi[m, :] = eta[k, m*D:(m+1)*D]
+            psi -= mu[:, np.newaxis]*mu*nu[k]
+            xi = eta[k, D*D+D]-D-2
+            sig = psi/(xi+D+1)
 
+            if (k < eta.shape[0]-1):
+                wt = stick*ab[0, k]/(ab[0, k]+ab[1, k])
+                stick = stick*ab[1, k]/(ab[0, k]+ab[1, k])
+            else:
+                wt = stick
 
+            xy  = mu[:, np.newaxis] + np.dot(np.linalg.cholesky(sig), np.vstack((np.cos(np.linspace(0, 2*np.pi, 100)), np.sin(np.linspace(0, 2*np.pi, 100)))))
+            plt.plot(xy[0, :], xy[1, :], lw=5, alpha=np.sqrt(wt), c='r')
+        plt.title('Dist'+dtype[j]+'-'+dist_tags[i])
