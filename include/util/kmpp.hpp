1f4c90b0f51acfc621f045b59f7ba7bbe1be8b5d
Trevor Campbell <trevor.d.campbell@gmail.com>
2015-05-11 14:16:25 -0400

    fixed a bug in kmpp

--- include/util/kmpp.hpp
+++ include/util/kmpp.hpp
@@ -14,13 +14,25 @@ std::vector<uint32_t> kmeanspp(MXd x, Func Dsq, uint32_t K, MXd c0, uint32_t K0,
 	assert (K>= K0);
 	std::vector<uint32_t> res;
 	std::vector<double> mindists(x.rows(), std::numeric_limits<double>::infinity());
+	std::vector<double> sampledDists(K);
+
+	//if K = K0, just return the empty set since we have everything already initialized
 	if (K == K0){
 		return res;
 	}
 	if (K0 == 0){
+		//K != K0 and K0 = 0 -- just initialize the first center to uniform random
 		std::uniform_int_distribution<> uniint(0, x.rows()-1);
 		res.push_back(uniint(rng));
+		//update the min dists
+		for (uint32_t j = 0; j < x.rows(); j++){
+			VXd x1 = x.row(j).transpose();
+			VXd x2 = x.row(res.back()).transpose();
+			double newdist = Dsq(x1, x2);
+			mindists[j] = mindists[j] < newdist ? mindists[j] : newdist;
+		}
 	} else {
+		//Otherwise K0 != 0 -- just compute the distances to all the K0 old centers first
 		for (uint32_t i = 0; i < K0; i++){
 			VXd x2 = c0.row(i).transpose();
 			for (uint32_t j = 0; j < x.rows(); j++){
@@ -29,19 +41,17 @@ std::vector<uint32_t> kmeanspp(MXd x, Func Dsq, uint32_t K, MXd c0, uint32_t K0,
 				mindists[j] = mindists[j] < newdist ? mindists[j] : newdist;
 			}
 		}
+	}
+	//for each new cluster, sample a center and update min dists
+	for (uint32_t i =K0; i < K; i++){
 		std::discrete_distribution<> disc(mindists.begin(), mindists.end());
 		res.push_back(disc(rng));
-	}
-	
-	for (uint32_t i =K0+1; i < K; i++){
 		for (uint32_t j = 0; j < x.rows(); j++){
 			VXd x1 = x.row(j).transpose();
 			VXd x2 = x.row(res.back()).transpose();
 			double newdist = Dsq(x1, x2);
 			mindists[j] = mindists[j] < newdist ? mindists[j] : newdist;
 		}
-		std::discrete_distribution<> disc(mindists.begin(), mindists.end());
-		res.push_back(disc(rng));
 	}
 	return res;
 }
